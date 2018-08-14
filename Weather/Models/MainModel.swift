f1f7ef74fc391a4ab5f2f374993481b2757a362e
adrian <adrian.ortiz@amplemind.com>
2018-08-14 09:57:02 -0500

    Initial Commit

---
+++ Weather/Models/MainModel.swift
@@ -0,0 +1,11 @@
+//
+//  MainModel.swift
+//  Weather
+//
+//  Created by Adrian Ortiz on 8/14/18.
+//  Copyright (c) 2018 Adrian Ortiz. All rights reserved.
+//
+import Foundation
+
+struct MainModel {
+}
---
+++ Weather/ViewControllers/MainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainViewController.swift
+//  Weather
+//
+//  Created by Adrian Ortiz on 8/14/18.
+//  Copyright (c) 2018 Adrian Ortiz. All rights reserved.
+//
+import UIKit
+
+class MainViewController: UIViewController {
+
+    var viewModel: MainViewModel?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Life cycle
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
